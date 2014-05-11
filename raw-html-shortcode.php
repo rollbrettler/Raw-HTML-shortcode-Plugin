2ff25b733a541728134c8bdbe1f242ed09e5443f
Tim Petter <tim@timpetter.de>
2014-05-11 15:11:59 +0200

    updater fixes

--- raw-html-shortcode.php
+++ raw-html-shortcode.php
@@ -3,7 +3,7 @@
  * Plugin Name: Raw HTML shortcode Plugin
  * Plugin URI: https://github.com/rollbrettler/Raw-HTML-shortcode-Plugin
  * Description: A simple Plugin to manage raw HTML content and include it with a shortcode 
- * Version: 0.1
+ * Version: 0.1.2
  * Author: Tim Petter
  * Author URI: https://timpetter.de/
  * License: MIT
@@ -13,3 +13,11 @@ define("RAW_HTML_URL", plugin_dir_url( __FILE__ ));
 define("RAW_HTML_PATH", plugin_dir_path( __FILE__ ));
 
 include_once "raw-html-shortcode.function.php";
+
+// Updater class
+if (!class_exists('BFIGitHubPluginUpdater')) {
+    require_once( 'BFIGitHubPluginUploader.php' );
+}
+if ( is_admin() ) {
+    new BFIGitHubPluginUpdater( __FILE__, 'rollbrettler', "Raw-HTML-shortcode-Plugin" );
+}
