d12051ead73b84fd01e1ac39c6a355a0e5d1191f
Tim Petter <tim@timpetter.de>
2014-05-19 18:21:26 +0200

    added path for use in template

--- module.php
+++ module.php
@@ -1,6 +1,6 @@
 <?php
 
-define("RAW_HTML_URL", "");
-define("RAW_HTML_PATH", "");
+define("RAW_HTML_URL", get_stylesheet_directory_uri() . "/modules/interobang-editor/");
+define("RAW_HTML_PATH", get_stylesheet_directory() . "/modules/interobang-editor/");
 
 include_once "raw-html-shortcode.function.php"
